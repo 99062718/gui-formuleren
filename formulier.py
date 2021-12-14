68bd1d8828a1bad8f9886836c084a61002f80a4c
tycho <99062718@mydavinci.nl>
2021-12-14 10:36:39 +0100

    finished up formulier.py
    i always love it when stupid ideas work out perfectly

--- formulier.py
+++ formulier.py
@@ -1,5 +1,6 @@
 import tkinter
 from tkinter import IntVar, StringVar, ttk, messagebox
+import random
 
 mainWindow = tkinter.Tk()
 mainWindow.configure(padx=50, pady=10)
@@ -18,22 +19,34 @@ naamEntry.grid(column=1, row=0, pady=20)
 
 #-----------------------------------------------------------------
 
-leeftijdLabel = tkinter.Label(text="leeftijd:")
-leeftijdLabel.grid(column=0, row=1)
+usernameLabel = tkinter.Label(text="Minecraft username:")
+usernameLabel.grid(column=0, row=1)
 
-leeftijd = IntVar(mainWindow, 0)
-leeftijdSpinbox = ttk.Spinbox(textvariable=leeftijd, from_=0, to=float("inf"))
-leeftijdSpinbox.grid(column=1, row=1, pady=20)
+username = StringVar(mainWindow, "")
+usernameEntry = ttk.Entry(
+    textvariable=username,
+    width=20
+)
+usernameEntry.grid(column=1, row=1, pady=20)
+
+#-----------------------------------------------------------------
+
+leeftijdLabel = tkinter.Label(text="leeftijd:")
+leeftijdLabel.grid(column=0, row=2)
+
+leeftijd = IntVar(mainWindow, 1)
+leeftijdSpinbox = ttk.Spinbox(textvariable=leeftijd, from_=1, to=float("inf"))
+leeftijdSpinbox.grid(column=1, row=2, pady=20)
 
 #-----------------------------------------------------------------
 
 computerFrame = tkinter.Frame()
-computerFrame.grid(column=0, row=2, columnspan=2)
+computerFrame.grid(column=0, row=3, columnspan=2)
 
 computerLabel = tkinter.Label(computerFrame, text="Wilt u uw computer meenemen of er een huren?")
 computerLabel.grid(column=0, row=0)
 
-computerButtonsValues = (("Ik neem mijn laptop mee", "laptop"), ("Ik neem mijn desktop mee", "desktop"), ("Ik huur een computer", "huur"))
+computerButtonsValues = (("Ik neem mijn laptop mee", "laptop"), ("Ik neem mijn desktop mee", "desktop"), ("Ik huur een computer", "gehuurd"))
 computerButtons = [1]
 computer = StringVar()
 num = 1
@@ -50,8 +63,22 @@ for values in computerButtonsValues:
     )
     num += 1
 
-
-
 #-----------------------------------------------------------------
+
+def submit():
+    ifValues = ((naam, ""), (username, ""), (leeftijd, 4), (computer, ""))
+    veld = 1
+    fault = False
+    for values in ifValues:
+        if values[0].get() <= values[1]:
+            fault = True
+            messagebox.showerror(message="veld {} is niet geldig".format(veld))
+        veld += 1
+    if fault == False:
+        messagebox.showinfo(message="Naam: {}\nUsername: {}\nLeeftijd: {}\nDevice: {}\nRegistratie code: {}".format(naam.get(), username.get(), leeftijd.get(), computer.get(), random.randint(10000, 99999)))
+
+
+submitButton = tkinter.Button(text="Submit", command=submit, width=30, bg="white", fg="black")
+submitButton.grid(column=0, row=4, columnspan=2)
 
 mainWindow.mainloop()
